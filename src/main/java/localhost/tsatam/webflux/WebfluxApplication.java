71740dd087a576391419667eed13f963fcdf35bc
Tanmay Satam <tsatam@umich.edu>
2019-02-10 15:08:13 -0500

    Add initial Spring WebFlux application

---
+++ src/main/java/localhost/tsatam/webflux/WebfluxApplication.java
@@ -0,0 +1,14 @@
+package localhost.tsatam.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxApplication.class, args);
+	}
+
+}
+
